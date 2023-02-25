d79430d3ebe4aedf1d224fd99cd5455299b8bbd0
Travis Horesh <horeshtr@gmail.com>
2023-02-25 08:55:36 -0500

    loaded gapminder and dplyr packages

--- datacamp_intro_to_tidyverse.R
+++ datacamp_intro_to_tidyverse.R
@@ -1,4 +1,8 @@
 # Datacamp - Introduction to the tidyverse
 
+if(!require(gapminder)) install.packages("gapminder")
+if(!require(dplyr)) install.packages("dplyr")
+
 library(gapminder)
 library(dplyr)
+
