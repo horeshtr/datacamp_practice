654196fcb52d749ed2420692d4ad56dbd7cbb8dc
Travis Horesh <horeshtr@gmail.com>
2023-02-25 09:01:23 -0500

    Completed practice for filter and arrange

--- datacamp_intro_to_tidyverse.R
+++ datacamp_intro_to_tidyverse.R
@@ -6,3 +6,29 @@ if(!require(dplyr)) install.packages("dplyr")
 library(gapminder)
 library(dplyr)
 
+
+# filter
+
+# filtering for one year
+gapminder %>%
+  filter(year == 2007)
+
+# filtering for one country
+gapminder %>%
+  filter(country = "United States")
+
+# filtering for multiple conditions
+gapminder %>%
+  filter(year == 2007, country == "United States")
+
+
+# arrange
+
+# sort by ascending GDP per capita
+gapminder %>%
+  arrange(gdpPercap)
+
+# sort by descending GDP per capita
+gapminder %>%
+  arrange(desc(gdpPercap))
+
